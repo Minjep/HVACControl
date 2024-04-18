c1dbafa48da33d2c052df170b05145ccd6bf6ae4
Jakob <jako7115@gmail.com>
2024-04-18 12:53:01 +0200

    .

--- Proccesing_filterData.py
+++ Proccesing_filterData.py
@@ -4,8 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta, timezone
 from AirmasterDataLib.loadData import load_data, load_telemetry, translate_telemetry
-from AirmasterDataLib.process.filter_data import getData,convertUnixToRealTime, convertUnixTimeAndFillMissingData, makePklFile, loadPklFile,adjustStartEndTime,removeNightAndWeekend,detect_and_filter_outliers,adjustResolution
-def create15minIntervals_csv(force=False):
+from AirmasterDataLib.process.filter_data import getData,convertUnixToRealTime, convertUnixTimeAndFillMissingData, makePklFile, loadPklFile,adjustStartEndTime,removeNightAndWeekend,detect_and_filter_outliers,adjustResolution,dataframe_to_csv
+def process_data_and_create_csv(force=False,createCSV=False):
     # create 15 min intervals and the other pkl files, if they do not exist
     if force or not os.path.exists('processedData.pkl'):
         print ("processedData not found, converting data to unix time and filling missing data...")
@@ -25,7 +25,7 @@ def create15minIntervals_csv(force=False):
         dataDay = loadPklFile("dayTimeData.pkl")
         var_to_be_detected = ['rqf','rqt','t_ai','t_ar','t_ao','t_aop','t_aio','t_vti','rh_ai','rh_ar','rh_ae','rh_ao','co2_ai','co2_ar','co2_ao']
         detect_and_filter_outliers(dataDay,var_to_be_detected,threshold=4,makeFile = True)
-    if force or not os.path.exists('finalData.csv'):
+    if force or createCSV:
         print("CSV file not found")
         dataFinal = loadPklFile("finalData.pkl")
         dataframe_to_csv(dataFinal)
@@ -33,10 +33,9 @@ def create15minIntervals_csv(force=False):
 
 
 def main():
-    create15minIntervals_csv(force=True)       
+    process_data_and_create_csv(force=False,createCSV=True)       
 
 if __name__ == "__main__":
-    
     main()
     
     
